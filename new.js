b906cb93033e64ad9a73e10ad0772d59b4f3d2fb
Henry Zhang <henry.alakazhang@gmail.com>
2017-04-29 16:53:12 +1000

    Searches for message chunks

--- new.js
+++ new.js
@@ -1,1 +1,20 @@
-alert("foo");
+$(document).on('click', '._4qba'/*'._5f0v._4wzs'*/, function(e) {
+    const messages = $('[aria-label="Messages"]').eq(0);
+    console.log(messages);
+    const messageList = messages.children('[id^="js_"]').eq(0);
+    console.log(messageList);
+    for (const i in messageList.children()) {
+        const child = messageList.children().eq(i);
+        if (child.prop('tagName') === 'DIV') {
+            // child.children('._3oh-_58nk').each(function() {
+            //     console.log(child.innerText);
+            //     child.css('background-color', 'green');
+            // });
+            console.log("found");
+            child.css('background-color', 'red');
+        } else if (child.prop('tagName') === 'H4') {
+            console.log("New time");
+            child.css('background-color', 'blue');
+        }
+    };
+});
